2f87a1054d1e2fb3e6fec12e9ce6bb43f3869486
bingFly <hanbing_code@163.com>
2017-03-17 00:44:12 +0800

    模型

--- iRime/Keyboard/DiamondKeyboard/DiamondKeyboardModel.swift
+++ iRime/Keyboard/DiamondKeyboard/DiamondKeyboardModel.swift
@@ -9,8 +9,162 @@
 import Foundation
 
 class DiamondKeyboardModel: NSObject {
-
+    
+    var defaultArray:NSArray?
+    var letterArray:NSArray?
+    var bottomArray:NSArray?
+    
     override init() {
-        super.init();
+        super.init()
+        self.configData()
+    }
+    
+    func configData() {
+        self.defaultArray = self.configDefaultDatasource()
+        self.letterArray = self.configLetterDatasource()
+        self.bottomArray = self.configBottomDatasource()
+    }
+    
+    func configBottomDatasource() -> NSArray {
+        let firstDatasource:NSMutableArray = NSMutableArray.init(capacity: 8)
+        
+        let key1:DiamondKey = DiamondKey.init(style: 0)
+        key1.firstText = "符"
+        firstDatasource.addObject(key1)
+        
+        let key2:DiamondKey = DiamondKey.init(style: 0)
+        key2.firstText = "123"
+        firstDatasource.addObject(key2)
+        
+        let key3:DiamondKey = DiamondKey.init(style: 1)
+        key3.imageName = "空格"
+        firstDatasource.addObject(key3)
+        
+        let key4:DiamondKey = DiamondKey.init(style: 3)
+        key4.imageName = "图标"
+        key4.text("中", secondText: "英")
+        firstDatasource.addObject(key4)
+        
+        let key5:DiamondKey = DiamondKey.init(style: 0)
+        key5.firstText = "换行"
+        firstDatasource.addObject(key5)
+        
+        return NSArray.init(array: firstDatasource)
+    }
+    
+    func configDefaultDatasource() -> NSArray {
+        let firstDatasource:NSMutableArray = NSMutableArray.init(capacity: 8)
+        
+        let key1:DiamondKey = DiamondKey.init(style: 0)
+        key1.firstText = "，"
+        firstDatasource.addObject(key1)
+        
+        let key2:DiamondKey = DiamondKey.init(style: 0)
+        key2.firstText = "。"
+        firstDatasource.addObject(key2)
+        
+        let key3:DiamondKey = DiamondKey.init(style: 0)
+        key3.firstText = "？"
+        firstDatasource.addObject(key3)
+        
+        let key4:DiamondKey = DiamondKey.init(style: 0)
+        key4.firstText = "！"
+        firstDatasource.addObject(key4)
+        
+        let key5:DiamondKey = DiamondKey.init(style: 0)
+        key5.firstText = "..."
+        firstDatasource.addObject(key5)
+        
+        let key6:DiamondKey = DiamondKey.init(style: 0)
+        key6.firstText = "~"
+        firstDatasource.addObject(key6)
+        
+        let key7:DiamondKey = DiamondKey.init(style: 0)
+        key7.firstText = "'"
+        firstDatasource.addObject(key7)
+        
+        let key8:DiamondKey = DiamondKey.init(style: 0)
+        key8.firstText = "、"
+        firstDatasource.addObject(key8)
+        
+        return NSArray.init(array: firstDatasource)
+    }
+    func configLetterDatasource() -> NSArray{
+        let firstDatasource:NSMutableArray = NSMutableArray.init(capacity: 12)
+        
+        let key1:DiamondKey = DiamondKey.init(style: 2)
+        key1.text("1", secondText: "@/.")
+        firstDatasource.addObject(key1)
+        
+        let key2:DiamondKey = DiamondKey.init(style: 2)
+        key2.text("2", secondText: "ABC")
+        firstDatasource.addObject(key2)
+        
+        let key3:DiamondKey = DiamondKey.init(style: 2)
+        key3.text("3", secondText: "DEF")
+        firstDatasource.addObject(key3)
+        
+        let key4:DiamondKey = DiamondKey.init(style: 1)
+        key4.imageName = "删除"
+        firstDatasource.addObject(key4)
+        
+        let key5:DiamondKey = DiamondKey.init(style: 2)
+        key5.text("4", secondText: "GHI")
+        firstDatasource.addObject(key5)
+        
+        let key6:DiamondKey = DiamondKey.init(style: 2)
+        key6.text("5", secondText: "JKL")
+        firstDatasource.addObject(key6)
+        
+        let key7:DiamondKey = DiamondKey.init(style: 2)
+        key7.text("6", secondText: "MNO")
+        firstDatasource.addObject(key7)
+        
+        let key8:DiamondKey = DiamondKey.init(style: 0)
+        key8.firstText = "换行"
+        firstDatasource.addObject(key8)
+        
+        let key9:DiamondKey = DiamondKey.init(style: 2)
+        key9.text("7", secondText: "PQRS")
+        firstDatasource.addObject(key9)
+        
+        let key10:DiamondKey = DiamondKey.init(style: 2)
+        key10.text("8", secondText: "TUV")
+        firstDatasource.addObject(key10)
+        
+        let key11:DiamondKey = DiamondKey.init(style: 2)
+        key11.text("9", secondText: "WXYZ")
+        firstDatasource.addObject(key11)
+        
+        let key12:DiamondKey = DiamondKey.init(style: 0)
+        key12.firstText = "0"
+        firstDatasource.addObject(key12)
+        
+        return NSArray.init(array: firstDatasource)
+    }
+}
+
+class DiamondKey:NSObject {
+    /**
+     style 
+     0 普通的显示文案
+     1 显示单独的图片
+     2 数字+字母
+     3 图片+文案
+ */
+    var style:NSInteger?
+    var firstText:NSString?
+    var secondText:NSString?
+    var imageName:NSString?
+    var letterArray:NSArray?
+    
+    init(style:NSInteger) {
+        super.init()
+        self.style = style
+    }
+    
+    func text(firstText:NSString,secondText:NSString) {
+        self.firstText = firstText
+        self.secondText = secondText
     }
 }
--- iRime/Keyboard/DiamondKeyboard/DiamondKeyboardView.swift
+++ iRime/Keyboard/DiamondKeyboard/DiamondKeyboardView.swift
@@ -8,17 +8,75 @@
 
 import UIKit
 
+//@protocol RimeNotificationDelegate <NSObject>
+//
+//@optional
+//- (void)onDeploymentStarted;
+//- (void)onDeploymentSuccessful;
+//- (void)onDeploymentFailed;
+//- (void)onSchemaChangedWithNewSchema:(NSString *)schema;
+//- (void)onOptionChangedWithOption:(XRimeOption)option value:(BOOL)value;
+//
+//@end
 
-class DiamondKeyboardView: UIView {
+func createTableView(fatherView:protocol<UITableViewDataSource, UITableViewDelegate>) -> UITableView {
+    let tableView: UITableView = UITableView.init(frame: CGRectZero, style: .Plain);
+    tableView.delegate = fatherView
+    tableView.dataSource = fatherView
+    tableView.backgroundColor = UIColor.blueColor()
+    return tableView
+}
 
+class DiamondKeyboardView: UIView,UITableViewDataSource,UITableViewDelegate {
+
+    var tableView: UITableView? = nil
+    var collectionView: UICollectionView? = nil
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = UIColor.redColor()
+        self.configUI()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    func configUI() {
+        self.tableView = createTableView(self)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell.init()
+    
+    }
+}
+
+class DiamondBottomView: UIView,UITableViewDataSource,UITableViewDelegate {
+
+    var tableView: UITableView? = nil
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func configUI() {
+        self.tableView = createTableView(self)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell.init()
         
     }
-    let iRimeItem: UIButton = UIButton(type: .Custom)
 }
