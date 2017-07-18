b0a232ed77fc18002d7a380b22302c22a7aeb5cc
wangyu <245033896@qq.com>
2017-07-18 23:31:25 +0800

    1.DEBUG 预编译宏并没生效,已更改, 2.增减全局log函数,方便log,只在debug下生效 3.简单的先把符号键盘模块化,然后继续完善

---
+++ iRime/Keyboard/tasty-imitation-keyboard/Demo/iRSymbolBoard/View/iRSymbolBoardBottomControlView.swift
@@ -0,0 +1,14 @@
+//
+//  iRSymbolBoardBottomControlView.swift
+//  iRime
+//
+//  Created by 王宇 on 2017/7/18.
+//  Copyright © 2017年 jimmy54. All rights reserved.
+//  底部控制条view
+
+import UIKit
+
+class iRSymbolBoardBottomControlView: UIView {
+
+
+}
---
+++ iRime/Keyboard/tasty-imitation-keyboard/Demo/iRSymbolBoard/View/iRSymbolBoardRightWordsView.swift
@@ -0,0 +1,14 @@
+//
+//  iRSymbolBoardRightWordsView.swift
+//  iRime
+//
+//  Created by 王宇 on 2017/7/18.
+//  Copyright © 2017年 jimmy54. All rights reserved.
+//  右侧展示符号的view
+
+import UIKit
+
+class iRSymbolBoardRightWordsView: UIView {
+
+
+}
