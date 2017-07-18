f1af7a365ee0a8d417cda223fc354d580433da8e
wangyu <245033896@qq.com>
2017-07-18 23:31:58 +0800

    符号键盘模块化

---
+++ iRime/Keyboard/tasty-imitation-keyboard/Demo/iRSymbolBoard/View/iRSymbolBoardContentView.swift
@@ -0,0 +1,21 @@
+//
+//  iRSymbolBoardContentView.swift
+//  iRime
+//
+//  Created by 王宇 on 2017/7/18.
+//  Copyright © 2017年 jimmy54. All rights reserved.
+//  符号键盘最底层的容器view
+
+import UIKit
+
+class iRSymbolBoardContentView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
