a9588633262612427bf0c30aaebc6967e6479af9
jimmy54 <jimmy54@126.com>
2017-07-27 00:55:24 +0800

    更新CandidateModel

---
+++ iRime/Keyboard/tasty-imitation-keyboard/Model/CandidateModel.h
@@ -0,0 +1,13 @@
+//
+//  CandidateModel.h
+//  iRime
+//
+//  Created by jimmy54 on 26/07/2017.
+//  Copyright © 2017 jimmy54. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CandidateModel : NSObject
+
+@end
---
+++ iRime/Keyboard/tasty-imitation-keyboard/Model/CandidateModel.m
@@ -0,0 +1,13 @@
+//
+//  CandidateModel.m
+//  iRime
+//
+//  Created by jimmy54 on 26/07/2017.
+//  Copyright © 2017 jimmy54. All rights reserved.
+//
+
+#import "CandidateModel.h"
+
+@implementation CandidateModel
+
+@end
